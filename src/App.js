aff39d201c870f3d3ff2bf07227a157b613a351f
Nikhita Thomas <nikhitathomas@Nikhitas-MacBook-Air.local>
2021-09-14 16:59:02 +0530

    Upload files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://restcountries.eu/rest/v2/region/asia')
+      .then(res => res.json())
+      .then(item => {
+        this.setState({
+          isLoaded: true,
+          items: item
+        })
+      })
+  }
+
+
+
+  render() {
+    var { isLoaded, items } = this.state
+
+    if (!isLoaded) {
+      return <div>Loading</div>
+    } else {
+      return (
+        <div className="App">
+          <ul className="items">
+            {items.map(item => (
+
+              <li className='item' key={item.id}>
+                <div className="names">
+                  <li>COUNTRY: {item.name} <br />CAPITAL: {item.capital}</li>
+
+                  <li>REGION: {item.region} <br /> SUBREGION: {item.subregion}</li>
+                  <li>POPULATION:{item.population}  <br />BORDERS: {item.borders}</li>
+                  <li>Languages: {item.languages.map(lang => (
+                    <li className='languages'>{lang.name}</li>
+                  ))}
+                  </li>
+                </div>
+                <div className="flags">
+                  <li><img className='flag' src={item.flag} /></li>
+                </div>
+
+
+              </li>
+            ))}
+          </ul>
+        </div >
+      )
+    }
+  }
 }
 
 export default App;
